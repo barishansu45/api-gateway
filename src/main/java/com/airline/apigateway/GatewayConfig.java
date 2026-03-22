deba63468d42521e49f4bd85163377a2c6ce63e6
barishansu45 <25070006080@stu.yasar.edu.tr>
2026-03-22 16:28:14 +0300

    Initial commit for api-gateway

---
+++ src/main/java/com/airline/apigateway/GatewayConfig.java
@@ -0,0 +1,22 @@
+package com.airline.apigateway;
+
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.Objects;
+
+@Configuration
+public class GatewayConfig {
+
+    @Bean(name = "userKeyResolver")
+    public KeyResolver userKeyResolver() {
+        return exchange -> Mono.just(
+                Objects.requireNonNull(exchange.getRequest().getRemoteAddress())
+                        .getAddress()
+                        .getHostAddress()
+        );
+    }
+}
